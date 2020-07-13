5619e086e0d826fa800ba0393597b0c05aa37a0a
Greg Schivley <greg.schivley@gmail.com>
2020-07-13 17:21:34 -0400

    Update to output files

--- select_clusters/test_clusters.py
+++ select_clusters/test_clusters.py
@@ -1,29 +1,43 @@
 from clusters import ClusterBuilder, load_metadata, read_parquet
 
-PATH = ".."
+PATH = "."
 SCENARIOS = [
     {
-        "region": "Region",
-        "ipm_regions": ["NENGREST", "PJM_Dom"],
-        "technology": "offshorewind",
-        "turbine_type": "fixed",
-        "pref_site": 1,
-        "min_capacity": None,
-        "max_clusters": 3,
+        "region": "CA_N",
+        "ipm_regions": ["WEC_CALN", "WECC_BANC"],
+        "technology": "wind",
+        # "turbine_type": "fixed",
+        # "pref_site": 1,
+        "min_capacity": 22000,
+        "max_clusters": 1,
+        "cap_multiplier": 1,
     },
     {
-        "region": "Region",
-        "ipm_regions": ["NENGREST", "PJM_Dom"],
-        "technology": "offshorewind",
-        "turbine_type": "fixed",
-        "pref_site": 0,
-        "min_capacity": 10,
-        "max_clusters": None,
+        "region": "CA_S",
+        "ipm_regions": ["WEC_SCE", "WEC_LADW", "WECC_SCE", "WEC_SDGE", "WECC_IID"],
+        "technology": "wind",
+        # "turbine_type": "fixed",
+        # "pref_site": 0,
+        "min_capacity": 45000,
+        "max_clusters": 10,
+    },
+    {
+        "region": "CA_N",
+        "ipm_regions": ["WEC_CALN", "WECC_BANC"],
+        "technology": "solarpv",
+        # "turbine_type": "fixed",
+        # "pref_site": 1,
+        "min_capacity": 200000,
+        "max_clusters": 4,
+        "cap_multiplier": 0.2,
     },
 ]
 
 builder = ClusterBuilder(PATH)
 builder.build_clusters(**SCENARIOS[0])
 builder.build_clusters(**SCENARIOS[1])
-builder.get_cluster_metadata()
-builder.get_cluster_profiles()
+metadata = builder.get_cluster_metadata()
+profiles = builder.get_cluster_profiles()
+
+metadata.to_csv("test_metadata.csv", index=False)
+profiles.to_csv("test_profiles.csv", index=False)
