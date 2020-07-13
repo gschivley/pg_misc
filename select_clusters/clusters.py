abefece78b35f6b719512d580bd14b03c0aa7200
ezwelty <ethan.welty@gmail.com>
2020-07-12 20:46:50 -0600

    Update cluster selection workflow to new clusters
    - Assumes capacity is in cluster metadata column `mw`
    - Assumes cluster groups are saved with group descriptors `*_group.json`

--- select_clusters/clusters.py
+++ select_clusters/clusters.py
@@ -1,12 +1,14 @@
-import itertools
-import pandas as pd
-import numpy as np
-import scipy.cluster.hierarchy
-import os
 import glob
+import itertools
 import json
+import os
+from typing import Any, Dict, List, Optional, Sequence
 
-WEIGHT = "gw"
+import numpy as np
+import pandas as pd
+import scipy.cluster.hierarchy
+
+WEIGHT = "mw"
 MEANS = [
     "lcoe",
     "interconnect_annuity",
@@ -17,47 +19,246 @@ MEANS = [
     "substation_metro_tx_miles",
     "site_metro_spur_miles",
 ]
-SUMS = ["area", "gw"]
+SUMS = ["area", "mw"]
 PROFILE_KEYS = ["cbsa_id", "cluster_level", "cluster"]
 HOURS_IN_YEAR = 8784
 
 
-def harvest_group_metadata(path="."):
-    """Harvest group metadata from directory."""
+class ClusterBuilder:
+    """
+    Builds clusters of resources.
+
+    Attributes:
+        groups (list of dict): Resource group metadata.
+            - `metadata` (str): Relative path to resource metadata file.
+            - `profiles` (str): Relative path to variable resource profiles file.
+            - `technology` (str): Resource type.
+            - ... and any additional (optional) keys.
+        clusters (list of dict): Resource clusters.
+            - `group` (dict): Resource group from :attr:`groups`.
+            - `kwargs` (dict): Arguments used to uniquely identify the group.
+            - `region` (str): Model region in which the clustering was performed.
+            - `clusters` (pd.DataFrame): Computed resource clusters.
+            - `profiles` (np.ndarray): Computed profiles for the resource clusters.
+    """
+
+    def __init__(self, path: str = ".") -> None:
+        """
+        Initialize with cluster group metadata.
+
+        Arguments:
+            path: Path to the directory containing the metadata files ('*_group.json').
+
+        Raises:
+            ValueError: Group metadata missing required keys.
+        """
+        self.groups = load_groups(path)
+        required = ("metadata", "profiles", "technology")
+        for g in self.groups:
+            missing = [k for k in required if k not in g]
+            if missing:
+                raise ValueError(f"Group metadata missing required keys {missing}: {g}")
+            g["metadata"] = os.path.abspath(os.path.join(path, g["metadata"]))
+            g["profiles"] = os.path.abspath(os.path.join(path, g["profiles"]))
+        self.clusters: List[dict] = []
+
+    def _test_clusters_exist(self) -> None:
+        if not self.clusters:
+            raise ValueError("No clusters have yet been computed")
+
+    def find_groups(self, **kwargs: Any) -> List[dict]:
+        """
+        Return the groups matching the specified arguments.
+
+        Arguments:
+            **kwargs: Arguments to match against group metadata.
+        """
+        return [
+            g
+            for g in self.groups
+            if all(k in g and g[k] == v for k, v in kwargs.items())
+        ]
+
+    def build_clusters(
+        self,
+        region: str,
+        ipm_regions: Sequence[str],
+        min_capacity: float = None,
+        max_clusters: int = None,
+        cap_multiplier: float = None,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Build and append resource clusters to the collection.
+
+        This method can be called as many times as desired before generating outputs.
+
+        Arguments:
+            region: Model region (used only to label results).
+            ipm_regions: IPM regions in which to select resources.
+            min_capacity: Minimum total capacity (MW). Resources are selected,
+                from lowest to highest levelized cost of energy (lcoe),
+                until the minimum capacity is just exceeded.
+                If `None`, all resources are selected for clustering.
+            max_clusters: Maximum number of resource clusters to compute.
+                If `None`, no clustering is performed; resources are returned unchanged.
+            cap_multiplier: Capacity multiplier applied to resource metadata.
+            **kwargs: Arguments to :meth:`get_groups` for selecting the resource group.
+
+        Raises:
+            ValueError: Arguments match multiple resource groups.
+        """
+        groups = self.find_groups(**kwargs)
+        if len(groups) > 1:
+            raise ValueError(f"Arguments match multiple resource groups: {groups}")
+        c: Dict[str, Any] = {}
+        c["group"] = groups[0]
+        c["kwargs"] = kwargs
+        c["region"] = region
+        metadata = load_metadata(c["group"]["metadata"], cap_multiplier=cap_multiplier)
+        c["clusters"] = build_clusters(
+            metadata,
+            ipm_regions=ipm_regions,
+            min_capacity=min_capacity,
+            max_clusters=max_clusters,
+        )
+        c["profiles"] = build_cluster_profiles(
+            c["group"]["profiles"], c["clusters"], metadata
+        )
+        self.clusters.append(c)
+
+    def get_cluster_metadata(self) -> Optional[pd.DataFrame]:
+        """
+        Return computed cluster metadata.
+
+        The following fields are added:
+        - `region` (str): Region label passed to :meth:`build_clusters`.
+        - `technology` (str): From resource metadata 'technology'.
+        - `cluster` (int): Unique identifier for each cluster to link to other outputs.
+
+        The following fields are renamed:
+        - `max_capacity`: From 'mw'.
+        - `area_km2`: From 'area'.
+
+        Raises:
+            ValueError: No clusters have yet been computed.
+        """
+        self._test_clusters_exist()
+        dfs = []
+        start = 0
+        for c in self.clusters:
+            df = c["clusters"].reset_index()
+            columns = [x for x in np.unique([WEIGHT] + MEANS + SUMS) if x in df]
+            n = len(df)
+            df = (
+                df[columns]
+                .assign(
+                    region=c["region"],
+                    technology=c["group"]["technology"],
+                    cluster=np.arange(start, start + n),
+                )
+                .rename(columns={"mw": "max_capacity", "area": "area_km2"})
+            )
+            dfs.append(df)
+            start += n
+        return pd.concat(dfs, axis=0, ignore_index=True, sort=False)
+
+    def get_cluster_profiles(self) -> Optional[pd.DataFrame]:
+        """
+        Return computed cluster profiles.
+
+        Uses multi-index columns ('region', 'Resource', 'cluster'), where
+        - `region` (str): Region label passed to :meth:`build_clusters`.
+        - `Resource` (str): From resource metadata 'technology'.
+        - `cluster` (int): Unique identifier for each cluster to link to other outputs.
+
+        The first column is the hour number.
+        Subsequent columns are the cluster profiles.
+
+        Raises:
+            ValueError: No clusters have yet been computed.
+        """
+        self._test_clusters_exist()
+        profiles = np.column_stack([c["profiles"] for c in self.clusters])
+        columns = []
+        start = 0
+        for c in self.clusters:
+            for _ in range(c["profiles"].shape[1]):
+                columns.append((c["region"], c["group"]["technology"], start))
+                start += 1
+        df = pd.DataFrame(profiles, columns=columns)
+        # Insert hour numbers into first column
+        df.insert(
+            loc=0,
+            column=("region", "Resource", "cluster"),
+            value=np.arange(HOURS_IN_YEAR),
+        )
+        return df
+
+
+def load_groups(path: str = ".") -> List[dict]:
+    """Load group metadata."""
     paths = glob.glob(os.path.join(path, "*_group.json"))
     groups = []
-    for path in paths:
-        with open(path, mode="r") as fp:
+    for p in paths:
+        with open(p, mode="r") as fp:
             groups.append(json.load(fp))
     return groups
 
 
-def format_metadata_inplace(df, cap_multiplier=None):
+def load_metadata(path: str, cap_multiplier: float = None) -> pd.DataFrame:
+    """Load resource metadata."""
+    df = pd.read_csv(path)
     if cap_multiplier:
-        df["gw"] = df["gw"] * cap_multiplier
+        df["mw"] = df["mw"] * cap_multiplier
     df.set_index("id", drop=False, inplace=True)
+    return df
 
 
-def format_profiles_inplace(df):
-    # Prepare index
-    # NOTE: Assumes already sorted by hour (ascending)
-    df.set_index(PROFILE_KEYS, inplace=True)
+def read_parquet(
+    path: str, columns: Sequence[str] = None, filters: Sequence[dict] = None
+) -> pd.DataFrame:
+    """Read parquet file."""
+    # NOTE: Due to pyarrow bug, cannot pass more than two dictionaries in `filters`.
+    dnf = None
+    if filters:
+        # Convert to disjunctive normal form (dnf)
+        dnf = [[(str(k), "=", v) for k, v in d.items()] for d in filters]
+    # https://arrow.apache.org/docs/python/generated/pyarrow.parquet.ParquetDataset.html
+    return pd.read_parquet(
+        path,
+        engine="pyarrow",
+        columns=columns,
+        filters=dnf,
+        # Needed for unpartitioned datasets
+        use_legacy_dataset=False,
+        memory_map=True,
+    )
 
 
-def build_clusters(metadata, ipm_regions, min_capacity=None, max_clusters=np.inf):
+def build_clusters(
+    metadata: pd.DataFrame,
+    ipm_regions: Sequence[str],
+    min_capacity: float = None,
+    max_clusters: int = None,
+) -> pd.DataFrame:
+    """Build resource clusters."""
+    if max_clusters is None:
+        max_clusters = np.inf
     if max_clusters < 1:
-        raise ValueError("Max clusters must be greater than zero")
+        raise ValueError("Max number of clusters must be greater than zero")
     df = metadata
     cdf = _get_base_clusters(df, ipm_regions).sort_values("lcoe")
+    if cdf.empty:
+        raise ValueError(f"No resources found in {ipm_regions}")
     if min_capacity:
         # Drop clusters with highest LCOE until min_capacity reached
-        end = cdf["gw"].cumsum().searchsorted(min_capacity) + 1
+        end = cdf["mw"].cumsum().searchsorted(min_capacity) + 1
         if end > len(cdf):
             raise ValueError(
-                f"Capacity in {ipm_regions} ({cdf['gw'].sum()} GW) less than minimum ({min_capacity} GW)"
+                f"Capacity in {ipm_regions} ({cdf['mw'].sum()} MW) less than minimum ({min_capacity} MW)"
             )
-        else:
-            cdf = cdf[:end]
+        cdf = cdf[:end]
     # Track ids of base clusters through aggregation
     cdf["ids"] = [[x] for x in cdf["id"]]
     # Aggregate clusters within each metro area (cbsa_id)
@@ -104,6 +305,7 @@ def build_clusters(metadata, ipm_regions, min_capacity=None, max_clusters=np.inf
     if len(cdf) > max_clusters:
         # Aggregate singleton metro area clusters
         Z = scipy.cluster.hierarchy.linkage(cdf[["lcoe"]].values, method="ward")
+        # TODO: Store mask in temporary table
         cdf["_keep"] = True
         for child_idx in Z[:, 0:2].astype(int):
             cdf.loc[child_idx, "_keep"] = False
@@ -117,54 +319,28 @@ def build_clusters(metadata, ipm_regions, min_capacity=None, max_clusters=np.inf
     return cdf[columns]
 
 
-def build_cluster_profiles(clusters, metadata, profiles):
+def build_cluster_profiles(
+    path: str, clusters: pd.DataFrame, metadata: pd.DataFrame
+) -> np.ndarray:
+    """Build cluster profiles."""
     results = np.zeros((HOURS_IN_YEAR, len(clusters)), dtype=float)
-    for i, ids in enumerate(clusters["ids"]):
-        idx = [tuple(x) for x in metadata.loc[ids, profiles.index.names].values]
-        capacities = profiles.loc[idx, "capacity_factor"].values.reshape(
-            HOURS_IN_YEAR, -1, order="F"
-        )
-        weights = metadata.loc[ids, "gw"].values
+    for i, cids in enumerate(clusters["ids"]):
+        weights = metadata.loc[cids, WEIGHT].values
         weights /= weights.sum()
-        results[:, i] = (capacities * weights).sum(axis=1)
+        for j, cid in enumerate(cids):
+            # Include ipm_region (partitioning column) to speed up filter
+            filters = (
+                metadata.loc[[cid], ["ipm_region"] + PROFILE_KEYS]
+                .reset_index(drop=True)
+                .to_dict("records")
+            )
+            # Assumes profile is already sorted by hour (ascending)
+            df = read_parquet(path, filters=filters, columns=["capacity_factor"])
+            results[:, i] += df["capacity_factor"].values * weights[j]
     return results
 
 
-def clusters_to_capacity_transmission_table(clusters, region, technology):
-    columns = [
-        "gw",
-        "area",
-        "lcoe",
-        "interconnect_annuity",
-        "spur_miles",
-        "tx_miles",
-        "site_metro_spur_miles",
-        "site_substation_spur_miles",
-        "substation_metro_tx_miles",
-        "ids",
-    ]
-    columns = [col for col in columns if col in clusters.columns]
-    return (
-        clusters[columns]
-        .assign(region=region, technology=technology, cluster=clusters.index)
-        .reset_index(drop=True)
-        .rename(columns={"gw": "max_capacity"})
-        .rename(columns={"area": "area_km2"})
-    )
-
-
-def clusters_to_variable_resource_profiles_table(
-    clusters, cluster_profiles, region, technology
-):
-    n_hours, n_clusters = cluster_profiles.shape
-    cols = {}
-    cols[("region", "Resource", "cluster")] = np.arange(n_hours) + 1
-    for i in range(n_clusters):
-        cols[(region, technology, clusters.index[i])] = cluster_profiles[:, i]
-    return pd.DataFrame(cols)
-
-
-def _get_base_clusters(df, ipm_regions):
+def _get_base_clusters(df: pd.DataFrame, ipm_regions: Sequence[str]) -> pd.DataFrame:
     return (
         df[df["ipm_region"].isin(ipm_regions)]
         .groupby("cbsa_id")
@@ -173,7 +349,7 @@ def _get_base_clusters(df, ipm_regions):
     )
 
 
-def _merge_children(df, **kwargs):
+def _merge_children(df: pd.DataFrame, **kwargs: Any) -> dict:
     parent = kwargs
     for key in SUMS:
         parent[key] = df[key].sum()
@@ -182,6 +358,6 @@ def _merge_children(df, **kwargs):
     return parent
 
 
-def _flat(*args):
-    args = [x if np.iterable(x) and not isinstance(x, str) else [x] for x in args]
-    return list(itertools.chain(*args))
+def _flat(*args: Sequence) -> list:
+    lists = [x if np.iterable(x) and not isinstance(x, str) else [x] for x in args]
+    return list(itertools.chain(*lists))
--- select_clusters/test_clusters.py
+++ select_clusters/test_clusters.py
@@ -1,139 +1,29 @@
-import pandas as pd
-from clusters import (
-    format_metadata_inplace,
-    format_profiles_inplace,
-    build_clusters,
-    build_cluster_profiles,
-    clusters_to_capacity_transmission_table,
-    clusters_to_variable_resource_profiles_table,
-)
-from pathlib import Path
+from clusters import ClusterBuilder, load_metadata, read_parquet
 
-# ---- Constants ----
-
-CWD = Path.cwd()
-
-METADATA_PATHS = {
-    "OnshoreWind": CWD.parent / "wind_base_cluster_metadata.csv",
-    "UtilityPV": CWD.parent / "solarpv_base_cluster_metadata.csv",
-}
-
-PROFILE_PATHS = {
-    "OnshoreWind": CWD.parent / "wind_base_cluster_profiles.parquet",
-    "UtilityPV": CWD.parent / "solarpv_base_cluster_profiles.parquet",
-}
-
-CAPACITY_MULTIPLIER = {
-    "OnshoreWind": 1,
-    "UtilityPV": 0.2,
-}
-
-SCENARIOS = {
-    "UtilityPV": {
-        "CA_N": {
-            "ipm_regions": ["WEC_CALN", "WECC_BANC"],
-            "max_clusters": 4,
-            "min_capacity": 200,
-        },
-        "CA_S": {
-            "ipm_regions": ["WEC_SCE", "WEC_LADW", "WECC_SCE", "WEC_SDGE", "WECC_IID"],
-            "max_clusters": 10,  # 5
-            "min_capacity": 150,
-        },
-        "WECC_N": {
-            "ipm_regions": ["WECC_ID", "WECC_MT", "WECC_NNV", "WECC_SNV", "WECC_UT"],
-            "max_clusters": 20,
-            "min_capacity": 250,
-        },
-        "WECC_NMAZ": {
-            "ipm_regions": ["WECC_AZ", "WECC_NM"],
-            "max_clusters": 15,
-            "min_capacity": 300,
-        },
-        # "WECC_PNW": {
-        #     "ipm_regions": [],
-        #     "max_clusters": 1,
-        #     "min_capacity": 4.5,
-        # },
-        "WECC_WYCO": {
-            "ipm_regions": ["WECC_WY", "WECC_CO"],
-            "max_clusters": 15,
-            "min_capacity": 180,
-        },
+PATH = ".."
+SCENARIOS = [
+    {
+        "region": "Region",
+        "ipm_regions": ["NENGREST", "PJM_Dom"],
+        "technology": "offshorewind",
+        "turbine_type": "fixed",
+        "pref_site": 1,
+        "min_capacity": None,
+        "max_clusters": 3,
     },
-    "OnshoreWind": {
-        "CA_N": {
-            "ipm_regions": ["WEC_CALN", "WECC_BANC"],
-            "max_clusters": 1,
-            "min_capacity": 22,
-        },
-        "CA_S": {
-            "ipm_regions": ["WEC_SCE", "WEC_LADW", "WECC_SCE", "WEC_SDGE", "WECC_IID"],
-            "max_clusters": 3,  # 10
-            "min_capacity": 45,
-        },
-        "WECC_N": {
-            "ipm_regions": ["WECC_ID", "WECC_MT", "WECC_NNV", "WECC_SNV", "WECC_UT"],
-            "max_clusters": 15,
-            "min_capacity": 100,
-        },
-        "WECC_NMAZ": {
-            "ipm_regions": ["WECC_AZ", "WECC_NM"],
-            "max_clusters": 15,
-            "min_capacity": 120,
-        },
-        # "WECC_PNW": {
-        #     "ipm_regions": [],
-        #     "max_clusters": 1,
-        #     "min_capacity": 6,
-        # },
-        "WECC_WYCO": {
-            "ipm_regions": ["WECC_WY", "WECC_CO"],
-            "max_clusters": 20,
-            "min_capacity": 200,
-        },
+    {
+        "region": "Region",
+        "ipm_regions": ["NENGREST", "PJM_Dom"],
+        "technology": "offshorewind",
+        "turbine_type": "fixed",
+        "pref_site": 0,
+        "min_capacity": 10,
+        "max_clusters": None,
     },
-}
+]
 
-# ---- Processing ----
-
-technology = "UtilityPV"
-region = "CA_N"
-scenario = SCENARIOS[technology][region]
-
-# Prepare resource metadata
-metadata = pd.read_csv(METADATA_PATHS[technology])
-format_metadata_inplace(metadata, cap_multiplier=CAPACITY_MULTIPLIER[technology])
-
-# Prepare resource profiles
-profiles = pd.read_parquet(
-    PROFILE_PATHS[technology],
-    engine="pyarrow",
-    filters=[[("ipm_region", "=", region)] for region in scenario["ipm_regions"]],
-    # NOTE: Needed for unpartitioned datasets
-    # https://arrow.apache.org/docs/python/generated/pyarrow.parquet.ParquetDataset.html
-    use_legacy_dataset=False,
-)
-format_profiles_inplace(profiles)
-
-# Build clusters
-clusters = build_clusters(metadata, **scenario)
-
-# Build cluster profiles
-cluster_profiles = build_cluster_profiles(clusters, metadata, profiles)
-
-# Export results as PowerGenome input
-tab1 = clusters_to_capacity_transmission_table(clusters, region, technology)
-tab1.to_csv(
-    f"{technology}_{region}_test_resource_capacity_spur_line.csv",
-    index=False,
-    float_format="%.3f",
-)
-tab2 = clusters_to_variable_resource_profiles_table(
-    clusters, cluster_profiles, region, technology
-)
-tab2.to_csv(
-    f"{technology}_{region}_test_variable_resource_profiles.csv",
-    index=False,
-    float_format="%.4f",
-)
+builder = ClusterBuilder(PATH)
+builder.build_clusters(**SCENARIOS[0])
+builder.build_clusters(**SCENARIOS[1])
+builder.get_cluster_metadata()
+builder.get_cluster_profiles()
